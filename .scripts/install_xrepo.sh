794730a917a7390fdc87f19389d5f3f3cd8edfc1
blackmanjaro <lw2957409438@gmail.com>
2022-03-03 18:01:01 +0800

    add myself reposity of xrepo-cmake

--- .scripts/install_xrepo.sh
+++ .scripts/install_xrepo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cmake_root_dir=$1
-xrepo_url="https://github.com/xmake-io/xrepo-cmake.git"
+xrepo_url="https://github.com/williamgoods/xrepo-cmake.git"
 xrepo_dir=$cmake_root_dir/.xrepo/
 
 bash $cmake_root_dir/.scripts/clone.sh $xrepo_url $xrepo_dir
